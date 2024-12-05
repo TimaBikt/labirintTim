43e818c33aef8314374c94d25667409f3db3a470
TimaBikt <bikt07timur@gmail.com>
2024-12-05 19:11:59 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1733407761,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ПлиточныйФон.png",
+		"images/Спрайт-sheet0.png",
+		"images/Картаплитки.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт4-sheet1.png",
+		"images/Спрайт4-sheet2.png",
+		"images/Спрайт5-sheet0.png",
+		"images/Спрайт5-sheet1.png",
+		"images/Спрайт5-sheet2.png",
+		"images/Спрайт6-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
